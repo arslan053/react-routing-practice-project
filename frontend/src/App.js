93148f30ae6d5bf47a449bbbf356b956be2d7709
Muhammad Arslan <arslan05342@gmail.com>
2024-11-07 10:08:28 +0500

    data s being fetched by loader now and also error is handled with the help of react-route-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
-import Events from './pages/Events';
+import Events, { loader as EventsLoader } from './pages/Events';
 import RootLayout from './pages/RootLayout';
 import EventsDetailPage from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import EventsNavigation from './components/EventsNavigation';
 import EventsLayout from './pages/EventsLayout';
+import { loader } from './pages/Events';
+import ErrorPage from './pages/Error';
 
 
 function App() {
   const router = createBrowserRouter([
-    {path: '/', element:<RootLayout />, children: [
+    {
+      path: '/',
+      element:<RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
       {index: true, element: <HomePage />},
       {path: '/events', element: <EventsLayout />, children: [
-        {index: true, element: <Events />},
+        {index: true, element: <Events />, loader: EventsLoader},
         {path: '/events/:eventId', element: <EventsDetailPage />},
         {path: '/events/new', element: <NewEventPage />},
         {path: '/events/:eventId/edit', element: <EditEventPage />},
@@ -27,3 +33,4 @@ function App() {
 }
 
 export default App;
+
